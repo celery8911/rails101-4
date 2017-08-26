313bfe4d30b2429f722e2f1918827ebcd6f1df4d
Celery8911 <celery8911@gmail.com>
2017-08-26 12:24:25 +0800

    implement group#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,6 +22,17 @@ class GroupsController < ApplicationController
    @group = Group.find(params[:id])
  end
 
+ def update
+   @group = Group.find(params[:id])
+
+   if @group.update(group_params)
+     redirect_to groups_path, notice: 'Upadate Success'
+   else
+     render :edit
+   end
+
+ end 
+
   private
 
   def group_params
